13d95b62eb7e2c6534c953986a2c796be5c64ecb
TwixxHub <e.liprimmer@gmail.com>
2025-12-11 09:13:04 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,16 @@
+-- [[ Main.lua ]] --
+-- This is the script stored on GitHub
+
+local StarterGui = game:GetService("StarterGui")
+
+-- Send a notification so we know it loaded
+StarterGui:SetCore("SendNotification", {
+    Title = "TwixxHub",
+    Text = "Main script loaded successfully!",
+    Duration = 5
+})
+
+print("[TwixxHub] The main script is running.")
+
+-- YOUR GUI CODE GOES HERE --
+-- local Library = loadstring(...)
