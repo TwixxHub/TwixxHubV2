fc766515309b36d913c120b0309b7184988b3f68
TwixxHub <e.liprimmer@gmail.com>
2025-12-11 09:16:57 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TwixxHub/TwixxHubV2/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TwixxHub/TwixxHubV2/main/Main.lua"))()
